a684c244bb7c4e00a4ff1e780ddfd22093ef7d71
Amanda Souza <amanda@amandasouza.app>
2019-12-09 00:03:29 +0200

    fix: change package name

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	s3minio "./s3minio"
+	minio "./minio"
 	"github.com/hashicorp/terraform/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: s3minio.Provider,
+		ProviderFunc: minio.Provider,
 	})
 }

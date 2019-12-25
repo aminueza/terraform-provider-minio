0fbf4e362065bf253b89b0a74f149810828aac16
Amanda Souza <amanda@amandasouza.app>
2019-12-25 19:03:52 -0300

    update madmin

--- madmin/user-commands.go
+++ madmin/user-commands.go
@@ -1,24 +1,8 @@
-/*
- * MinIO Cloud Storage, (C) 2018 MinIO, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package madmin
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 
@@ -39,6 +23,7 @@ type UserInfo struct {
 	SecretKey  string        `json:"secretKey,omitempty"`
 	PolicyName string        `json:"policyName,omitempty"`
 	Status     AccountStatus `json:"status"`
+	MemberOf   []string      `json:"memberOf,omitempty"`
 }
 
 // RemoveUser - remove a user.
@@ -51,7 +36,7 @@ func (adm *AdminClient) RemoveUser(accessKey string) error {
 		queryValues: queryValues,
 	}
 
-	// Execute DELETE on /minio/admin/v1/remove-user to remove a user.
+	// Execute DELETE on /minio/admin/v2/remove-user to remove a user.
 	resp, err := adm.executeMethod("DELETE", reqData)
 
 	defer closeResponse(resp)
@@ -72,7 +57,7 @@ func (adm *AdminClient) ListUsers() (map[string]UserInfo, error) {
 		relPath: "/v1/list-users",
 	}
 
-	// Execute GET on /minio/admin/v1/list-users
+	// Execute GET on /minio/admin/v2/list-users
 	resp, err := adm.executeMethod("GET", reqData)
 
 	defer closeResponse(resp)
@@ -95,6 +80,40 @@ func (adm *AdminClient) ListUsers() (map[string]UserInfo, error) {
 	}
 
 	return users, nil
+}
+
+// GetUserInfo - get info on a user
+func (adm *AdminClient) GetUserInfo(name string) (u UserInfo, err error) {
+	queryValues := url.Values{}
+	queryValues.Set("accessKey", name)
+
+	reqData := requestData{
+		relPath:     "/v1/user-info",
+		queryValues: queryValues,
+	}
+
+	// Execute GET on /minio/admin/v2/user-info
+	resp, err := adm.executeMethod("GET", reqData)
+
+	defer closeResponse(resp)
+	if err != nil {
+		return u, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return u, httpRespToErrorResponse(resp)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return u, err
+	}
+
+	if err = json.Unmarshal(b, &u); err != nil {
+		return u, err
+	}
+
+	return u, nil
 }
 
 // SetUser - sets a user info.
@@ -129,7 +148,7 @@ func (adm *AdminClient) SetUser(accessKey, secretKey string, status AccountStatu
 		content:     econfigBytes,
 	}
 
-	// Execute PUT on /minio/admin/v1/add-user to set a user.
+	// Execute PUT on /minio/admin/v2/add-user to set a user.
 	resp, err := adm.executeMethod("PUT", reqData)
 
 	defer closeResponse(resp)
@@ -149,32 +168,6 @@ func (adm *AdminClient) AddUser(accessKey, secretKey string) error {
 	return adm.SetUser(accessKey, secretKey, AccountEnabled)
 }
 
-// SetUserPolicy - adds a policy for a user.
-func (adm *AdminClient) SetUserPolicy(accessKey, policyName string) error {
-	queryValues := url.Values{}
-	queryValues.Set("accessKey", accessKey)
-	queryValues.Set("name", policyName)
-
-	reqData := requestData{
-		relPath:     "/v1/set-user-policy",
-		queryValues: queryValues,
-	}
-
-	// Execute PUT on /minio/admin/v1/set-user-policy to set policy.
-	resp, err := adm.executeMethod("PUT", reqData)
-
-	defer closeResponse(resp)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return httpRespToErrorResponse(resp)
-	}
-
-	return nil
-}
-
 // SetUserStatus - adds a status for a user.
 func (adm *AdminClient) SetUserStatus(accessKey string, status AccountStatus) error {
 	queryValues := url.Values{}
@@ -186,7 +179,7 @@ func (adm *AdminClient) SetUserStatus(accessKey string, status AccountStatus) er
 		queryValues: queryValues,
 	}
 
-	// Execute PUT on /minio/admin/v1/set-user-status to set status.
+	// Execute PUT on /minio/admin/v2/set-user-status to set status.
 	resp, err := adm.executeMethod("PUT", reqData)
 
 	defer closeResponse(resp)

2f1c7edeed2fb8b10f75aeef82f6392c5f33abfa
nolte <nolte07@googlemail.com>
2020-01-26 17:19:42 +0100

    renamed minio_iam_user_policy to minio_iam_user_policy_attachment, more aws like ...

--- minio/provider.go
+++ minio/provider.go
@@ -50,9 +50,9 @@ func Provider() terraform.ResourceProvider {
 			// "minio_s3_file":   resourceMinioFile(),
 			"minio_iam_group": resourceMinioIAMGroup(),
 			//"minio_iam_group_membership": resourceMinioIAMGroupMembership(),
-			"minio_iam_user":        resourceMinioIAMUser(),
-			"minio_iam_policy":      resourceMinioIAMPolicy(),
-			"minio_iam_user_policy": resourceMinioIAMUserPolicy(),
+			"minio_iam_user":                   resourceMinioIAMUser(),
+			"minio_iam_policy":                 resourceMinioIAMPolicy(),
+			"minio_iam_user_policy_attachment": resourceMinioIAMUserPolicyAttachment(),
 		},
 
 		ConfigureFunc: providerConfigure,

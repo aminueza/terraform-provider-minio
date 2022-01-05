6643864a526df5c74ac1cc8920009746f11a6a69
Jonathan Buch <jbuch@synyx.de>
2022-01-05 17:15:43 +0100

    ilm, revise import ordering

--- minio/resource_minio_ilm_rule.go
+++ minio/resource_minio_ilm_rule.go
@@ -2,10 +2,11 @@ package minio
 
 import (
 	"context"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/minio/minio-go/v7/pkg/lifecycle"
-	"log"
 )
 
 func resourceMinioILMRule() *schema.Resource {

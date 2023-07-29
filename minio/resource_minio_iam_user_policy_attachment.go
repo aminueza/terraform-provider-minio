8b48f909f7d9702c2e9a3cc9fafa2e418f45e3e5
Adphi <philippe.adrien.nousse@gmail.com>
2023-07-29 16:55:02 +0200

    fix minio_iam_user_policy_attachment: Error: Provider produced inconsistent result after apply (fix #494)
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- minio/resource_minio_iam_user_policy_attachment.go
+++ minio/resource_minio_iam_user_policy_attachment.go
@@ -13,6 +13,8 @@ import (
 	"github.com/minio/madmin-go"
 )
 
+var userPolicyAttachmentLock = NewMutexKV()
+
 func resourceMinioIAMUserPolicyAttachment() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: minioCreateUserPolicyAttachment,
@@ -44,12 +46,16 @@ func minioCreateUserPolicyAttachment(ctx context.Context, d *schema.ResourceData
 	var policyName = d.Get("policy_name").(string)
 	minioAdmin := meta.(*S3MinioClient).S3Admin
 
+	userPolicyAttachmentLock.Lock(userName)
+	defer userPolicyAttachmentLock.Unlock(userName)
+
 	policies, err := minioReadUserPolicies(ctx, minioAdmin, userName)
 	if err != nil {
 		return err
 	}
 	if !Contains(policies, policyName) {
 		policies = append(policies, policyName)
+		log.Printf("[DEBUG] Attaching policy %s to user: %s (%v)", policyName, userName, policies)
 		err := minioAdmin.SetPolicy(ctx, strings.Join(policies, ","), userName, false)
 		if err != nil {
 			return NewResourceError("unable to Set User policy", userName+" "+policyName, err)
@@ -58,13 +64,19 @@ func minioCreateUserPolicyAttachment(ctx context.Context, d *schema.ResourceData
 
 	d.SetId(id.PrefixedUniqueId(fmt.Sprintf("%s-", userName)))
 
-	return minioReadUserPolicyAttachment(ctx, d, meta)
+	return doMinioReadUserPolicyAttachment(ctx, d, meta, userName, policyName)
 }
-
 func minioReadUserPolicyAttachment(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	minioAdmin := meta.(*S3MinioClient).S3Admin
 	var userName = d.Get("user_name").(string)
 	var policyName = d.Get("policy_name").(string)
+
+	userPolicyAttachmentLock.Lock(userName)
+	defer userPolicyAttachmentLock.Unlock(userName)
+
+	return doMinioReadUserPolicyAttachment(ctx, d, meta, userName, policyName)
+}
+func doMinioReadUserPolicyAttachment(ctx context.Context, d *schema.ResourceData, meta interface{}, userName, policyName string) diag.Diagnostics {
+	minioAdmin := meta.(*S3MinioClient).S3Admin
 
 	policies, errUser := minioReadUserPolicies(ctx, minioAdmin, userName)
 	if errUser != nil {
@@ -90,6 +102,9 @@ func minioDeleteUserPolicyAttachment(ctx context.Context, d *schema.ResourceData
 	var userName = d.Get("user_name").(string)
 	var policyName = d.Get("policy_name").(string)
 
+	userPolicyAttachmentLock.Lock(userName)
+	defer userPolicyAttachmentLock.Unlock(userName)
+
 	policies, err := minioReadUserPolicies(ctx, minioAdmin, userName)
 	if err != nil {
 		return err
@@ -100,6 +115,7 @@ func minioDeleteUserPolicyAttachment(ctx context.Context, d *schema.ResourceData
 		return nil
 	}
 
+	log.Printf("[DEBUG] Detaching policy %s from user: %s (%v)", policyName, userName, newPolicies)
 	errIam := minioAdmin.SetPolicy(ctx, strings.Join(newPolicies, ","), userName, false)
 	if errIam != nil {
 		return NewResourceError("unable to delete user policy", userName, errIam)
@@ -143,6 +159,9 @@ func minioReadUserPolicies(ctx context.Context, minioAdmin *madmin.AdminClient, 
 		if !isLDAPUser || !errUserIsResponse || !strings.EqualFold(errUserResponse.Code, "XMinioAdminNoSuchUser") {
 			return nil, NewResourceError("failed to load user Infos", userName, errUser)
 		}
+	}
+	if userInfo.PolicyName == "" {
+		return nil, nil
 	}
 	return strings.Split(userInfo.PolicyName, ","), nil
 }

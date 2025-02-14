39f1ed5b2122e64e0a98974a8a53645d9587b2b8
Victor Nogueira <felladrin@gmail.com>
2025-02-14 10:30:56 +0100

    Add reference to the issue explaining why we need to keep the empty-policy check

--- minio/resource_minio_s3_bucket.go
+++ minio/resource_minio_s3_bucket.go
@@ -278,6 +278,7 @@ func minioSetBucketACL(ctx context.Context, bucketConfig *S3MinioBucket) diag.Di
 		return NewResourceError("unsupported ACL", bucketConfig.MinioACL, errors.New("(valid acl: private, public-write, public-read, public-read-write, public)"))
 	}
 
+	// Only some providers support bucket policies, so we skip setting a policy if the bucket policy is empty. See issue #608.
 	if policyString != "" {
 		if err := bucketConfig.MinioClient.SetBucketPolicy(ctx, bucketConfig.MinioBucket, policyString); err != nil {
 			log.Printf("%s", NewResourceErrorStr("unable to set bucket policy", bucketConfig.MinioBucket, err))

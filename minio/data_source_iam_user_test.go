192233098492e88eaf7140bcd7a2dfa911e2d9f8
aminueza <aminueza@users.noreply.github.com>
2026-04-06 14:08:15 -0300

    test(iam): add test for policy_names and member_of_groups population
    Adds a test scenario that attaches a policy and group to the user,
    then verifies the data source returns them in policy_names and
    member_of_groups.

--- minio/data_source_iam_user_test.go
+++ minio/data_source_iam_user_test.go
@@ -46,6 +46,31 @@ func TestAccDataSourceMinioIAMUser_disabled(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceMinioIAMUser_withPolicyAndGroup(t *testing.T) {
+	userName := "tfacc-" + acctest.RandString(8)
+	userSecret := "s3cr3t-" + acctest.RandString(16)
+	groupName := "tfacc-grp-" + acctest.RandString(8)
+	policyName := "tfacc-pol-" + acctest.RandString(8)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceIAMUserWithPolicyAndGroupConfig(userName, userSecret, groupName, policyName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "id", userName),
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "status", "enabled"),
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "policy_names.#", "1"),
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "policy_names.0", policyName),
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "member_of_groups.#", "1"),
+					resource.TestCheckResourceAttr("data.minio_iam_user.this", "member_of_groups.0", groupName),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceIAMUserConfig(name, secret string) string {
 	return `
 provider "minio" {}
@@ -73,6 +98,52 @@ resource "minio_iam_user" "test" {
 
 data "minio_iam_user" "this" {
   name = minio_iam_user.test.name
+}
+`
+}
+
+func testAccDataSourceIAMUserWithPolicyAndGroupConfig(name, secret, group, policy string) string {
+	return `
+provider "minio" {}
+
+resource "minio_iam_user" "test" {
+  name   = "` + name + `"
+  secret = "` + secret + `"
+}
+
+resource "minio_iam_policy" "test" {
+  name   = "` + policy + `"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:GetObject"]
+      Resource = ["arn:aws:s3:::*"]
+    }]
+  })
+}
+
+resource "minio_iam_user_policy_attachment" "test" {
+  user_name   = minio_iam_user.test.name
+  policy_name = minio_iam_policy.test.name
+}
+
+resource "minio_iam_group" "test" {
+  name = "` + group + `"
+}
+
+resource "minio_iam_group_user_attachment" "test" {
+  group_name = minio_iam_group.test.name
+  user_name  = minio_iam_user.test.name
+}
+
+data "minio_iam_user" "this" {
+  name = minio_iam_user.test.name
+
+  depends_on = [
+    minio_iam_user_policy_attachment.test,
+    minio_iam_group_user_attachment.test,
+  ]
 }
 `
 }
